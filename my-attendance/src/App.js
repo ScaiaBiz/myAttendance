cb6c4dc07180943e5378f551108592a0964e9f55
Emilio <toschi17@yahoo.i>
2022-10-07 19:13:41 +0200

    Impostato progetto

--- my-attendance/src/App.js
+++ my-attendance/src/App.js
@@ -1,7 +1,18 @@
-import './App.css';
+import classes from './App.module.css';
 
 function App() {
-	return <div className='App'>This is my app</div>;
+	return (
+		<div className={classes.App}>
+			<div className={classes.menu}>
+				<nav>
+					<div className={classes.menu__element}>Timbrature</div>
+					<div className={classes.menu__element}>Impostazioni</div>
+				</nav>
+				<div>Login / Logout</div>
+			</div>
+			<div className={classes.content}></div>
+		</div>
+	);
 }
 
 export default App;
